346f08c9c270b913c662f75635e340bc472f5ce2
Rohita9 <32105450+Rohita9@users.noreply.github.com>
2017-09-19 13:52:36 -0500

    Commiting API application

---
+++ Desktop/RestSpringBoot /src/main/java/com/fileupload/RestSpringBoot/Application.java
@@ -0,0 +1,10 @@
+package com.fileupload.RestSpringBoot;
+
+
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "You can use this AP" );
+    }
+}
